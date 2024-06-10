41c8f1cb8c8133c43aae592bd881055126db5ab7
Hq3r <102521264+Hq3r@users.noreply.github.com>
2024-06-10 19:48:07 +0200

    Update sellForErg.ts

--- src/lib/contract/sellForErg.ts
+++ src/lib/contract/sellForErg.ts
@@ -1,8 +1,9 @@
 export const sellForErg = `
 {
     val priceNanoErg     	= SELF.R4[Long].get         // token sale price in nanoErg
-    val sellerPK 			= SELF.R5[SigmaProp].get // Public Key of the token seller
-    val devPK			    = SELF.R6[SigmaProp].get // dev Public Key
+    val sellerPK 			= SELF.R5[SigmaProp].get    // Public Key of the token seller
+    val devPK			    = SELF.R6[SigmaProp].get    // dev Public Key
+    val cancelOrder         = 10000000L                 // cancel order value in nanoErg (0.01 ERG)
 
     val feeDenom 			= 100000L
     val feeNum   			= 1000L                     // 1% fee
@@ -11,8 +12,11 @@ export const sellForErg = `
     val sellerHappy			= (OUTPUTS(0).value >= priceNanoErg - feeInNanoErg) && (OUTPUTS(0).propositionBytes == sellerPK.propBytes)
     val feePaid				= (feeInNanoErg == 0) ||
                               (OUTPUTS(1).value >= feeInNanoErg && OUTPUTS(1).propositionBytes == devPK.propBytes)
-    sigmaProp(onlyOneBoxSpent && sellerHappy && feePaid) || sellerPK
-}`
+    val cancelOrderValid    = ((OUTPUTS(0).value == cancelOrder) && (OUTPUTS(0).propositionBytes == sellerPK.propBytes) && (OUTPUTS(1).propositionBytes == devPK.propBytes))
 
-export const CONTRACT  = '7BPUsYJNMQp6zCrqGw8qeWqkxdRxd3WYiZxDsNaKdnRyhdGabXEi2bd1PTSNQGSWcCfZkSobKQ9Ua4fvi3b5Siaa65mfnVw4aLzYPUvazLR31P5X2tTy3P5fy93aBKe8Qk3x4oSb5zZfBuAV2nPnHXMqzfzJ1yVEewFBGza3wLwuaQtFU';
-export const DEV_PK    = '9ffXZz5AovJvapPo63TGwdNaRPMUiHo2UkqGavmDGzrUERY9qJ3'
+    sigmaProp((onlyOneBoxSpent && sellerHappy && feePaid) || cancelOrderValid) || sellerPK
+}
+`
+
+export const CONTRACT  = '6yz51NUtPRmyfaGWvEPfcCRTeujKs9EpEnz7Yo2oxCbqWtUcyyjLenqzCz31MmwnLQwNc4QAA59M9pWZhczJhW2YPEK5FWsDPC526SsKg1id4q1Y3vzfJHcC7kviQLvM3anrvscKiucZkw5fpHkc8zNoEcS6opQ1ZpyNbrdWrJEi1MyESxk3GEEf5Tmt6iceXXVYnquo2Eddetdn5j4cRzzuqnRvgJvtnJGeHJ9G5';
+export const DEV_PK    = '9fCMmB72WcFLseNx6QANheTCrDjKeb9FzdFNTdBREt2FzHTmusY'
